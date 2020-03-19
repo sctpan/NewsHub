8adb53da312b76f2484fc406c09100e48540c05b
sctpan <sctpan@qq.com>
2020-03-18 21:02:18 -0700

    update

--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+
 router.get('/guardian', function(req, res) {
     let apiKey = '43326aea-0a7a-45e4-a0f6-7fc14af48780';
     let guardianUrl = 'https://content.guardianapis.com/';
@@ -22,10 +23,31 @@ router.get('/guardian', function(req, res) {
         params: params
     }).then(function (response) {
         data = response.data;
-        res.json(data);
-    }).catch(function (error) {
-        res.send(error);
+        returnNewsList = getGuardianNews(data)
+        res.json({'news': returnNewsList});
     })
 });
+
+function getGuardianNews(data) {
+    let newsList = data.response.results;
+    let processedNewsList = [];
+    for(var i=0; i<newsList.length; i++) {
+        let news = newsList[i];
+        let processedNews = {};
+        processedNews.id = news.id;
+        processedNews.title = news.webTitle;
+        let lastAssetIndex = news.blocks.main.elements[0].assets.length - 1;
+        if(lastAssetIndex < 0) {
+            processedNews.image = 'https://assets.guim.co.uk/images/eada8aa27c12fe2d5afa3a89d3fbae0d/fallback-logo.png';
+        } else {
+            processedNews.image = news.blocks.main.elements[0].assets[lastAssetIndex].file;
+        }
+        processedNews.section = news.sectionId;
+        processedNews.date = news.webPublicationDate;
+        processedNews.description = news.blocks.body[0].bodyTextSummary;
+        processedNewsList.push(processedNews);
+    }
+    return processedNewsList;
+}
 
 module.exports = router;
